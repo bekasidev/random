1a3817557a6e0c4f45c3619fe65193906db440e0
Maksum Rifai <maksum.rifai@gmail.com>
2020-10-22 23:43:42 +0700

    Create skrip.js

---
+++ skrip.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  var peserta = [
+    {
+        "id": "1",
+        "text": "Teks 1",
+        "nama": "Nama 1"
+    },
+    {
+        "id": "2",
+        "text": "Teks 2"
+        "author": "Nama 2"
+    },
+    {
+        "id": "3",
+        "text": "Teks 3",
+        "author": "Nama 3"
+    },    
+    ]
+    
+    $.ajax({
+    dataType: 'json'
+    })
+    var qty =peserta.length;
+    $('#main #text').html('"'+peserta[0].text+'"');
+    $('#main #nama').html(peserta[0].nama);
+    $("#main #quoteNum").html(quotes.indexOf(quotes[0])+1+' of '+qty);
+    
+    //console.log(quotes[randomNum]);
+    $('#generate').on('click', function(){
+    var randomNum = Math.floor(Math.random() * (qty - 0 + 1) + 0);
+    $('#main #text').html('"'+peserta[randomNum].text+'"');
+    $('#main #nama').html(peserta[randomNum].nama);
+    $("#main #quoteNum").html(peserta.indexOf(peserta[randomNum])+1+' of '+qty);
+    });
